4f8a4b48a75e5bcd69eeb9040755919116551863
Kanishk <rawat27kanishk@gmail.com>
2022-07-06 10:20:34 +0530

    modification in StackImplimation of Linked List

--- C/DSA/linkedList/StackImplimationofLinkedList.c
+++ C/DSA/linkedList/StackImplimationofLinkedList.c
@@ -26,12 +26,29 @@ void insertionAtTheBeginning(int n){                  // Insertion
     }
     start = Head;
     
+
 }
-void DeletionAtTheBeginning(){
+void push(){
+    p = (struct node*) malloc(sizeof(struct node));        // Address of the first node
+    printf("Enter in The Info Part :- ");     // Adding info to that node
+    scanf("%d",&p->info);                                  
+    if(start==NULL){                                       // If The node is first
+        start = p;                                           // Start = p
+        Head  = p;                                          // Head = p 
+        Head->link =NULL;
+    }
+    else{
+        p->link = Head;
+        Head =p;
+    }
+    start = Head;
+
+}
+void pop(){
     temp = Head;
     Head = Head->link;
     start = Head;
-    printf("Deleted Node = [%d | %p]\n",temp->info,temp->link);
+    printf("\nDeleted Node = [%d | %p]\n",temp->info,temp->link);
     free(temp);
 }
 void Display(){
@@ -43,18 +60,38 @@ void Display(){
         Head=Head->link;
     }
     printf("[End]");
+    Head = start;
     
     
 }
 
 int main(){
-    int n;
-    printf("Enter the Number of Nodes :- ");
-    scanf("%d",&n);
-    insertionAtTheBeginning(n);
-    Display();
-    Head = start;
+    int n=1,answer;
+    while(n==1){
     printf("\n");
-    DeletionAtTheBeginning();
-    Display();
+    printf("1.Push\n");
+    printf("2.Pop\n");
+    printf("3.Display\n");
+    printf("4.Exit\n");
+    printf("Enter the Answer :- ");
+    scanf("%d",&answer);
+    switch (answer)
+    {
+    case 1:
+        push();
+        break;
+    case 2:
+        pop();
+        break;
+    case 3:
+        Display();
+        break;
+    case 4:
+        n=0;
+        break;
+    default:
+        n=0;
+        break;
+    }
+    }
 }
