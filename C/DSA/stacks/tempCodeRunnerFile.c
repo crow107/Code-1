d412ec9ec352acf5d0280420654bce688b09b3f4
Kanishk <rawat27kanishk@gmail.com>
2022-07-10 01:20:00 +0530

    added Infix To postfix

--- C/DSA/stacks/tempCodeRunnerFile.c
+++ C/DSA/stacks/tempCodeRunnerFile.c
@@ -1,21 +1,4 @@
-#include<stdio.h>
-
-int main()
-{
-
-    int i, n;      //Traversal
-    int arr[10];     
-    printf("\nEnter the number of elements in the array: ");
-	scanf("%d",&n);
-	printf("\nEnter elements of array: \n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-    printf("\n The array elements are: ");
-	for(i=0;i<n;i++)
-    {
-		printf("\n arr[%d]= %d", i, arr[i]);
-    }
-     return 0;
-}
+else if(name[i] == '('){
+            stack[++top] = name[i];
+            
+        }
