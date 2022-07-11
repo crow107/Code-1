5d3b400b7843aeca06856d4f110b33bb4c4c2fa7
Kanishk <rawat27kanishk@gmail.com>
2022-07-11 23:01:35 +0530

    added trees

---
+++ C/DSA/trees/tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+void display(struct treeNode * mover){
+//     printf("\t %d \t\n",root->info);
+//     if(mover->leftTreeNode!=NULL|| mover->rightTreeNode !=NULL){
+//         if(mover->leftTreeNode !=NULL){
+//             display(mover->leftTreeNode)
+//         }
+//         if(mover->rightTreeNode !=NULL){
+//             display(mover->rightTreeNode);
+//         }
+//     }
+
+
+// }
